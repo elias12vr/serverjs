b4547b16d7f856db3a8306f5f6f1f608faf483a6
elias12vr <yoshytony0@gmail.com>
2024-09-03 15:06:21 -0600

    first commit

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ const http = require('http')
 
 const hostname = '127.0.0.1'
 const port = 3000
-const server = http.createServer((req, res));
+
+//crear el servidor con el estado 200 y texto en plano
+const server = http.createServer((req, res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text-plain')
+    res.end('Servidor funcionando en node \n')
+});
+//mandar a llamar el servidor
+server.listen(port, hostname,()=>{
+    console.log('el servidor node se esta corriendo en http://127.0.0.1:3000')
+})
